1330842cb22656a011c5e00347604769300eec9f
HaHoang <bhoangha.hp@gmail.com>
2023-10-06 19:02:44 +0700

    feat: auto-train Bot learning

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,54 @@
+from pathlib import Path
+
 from prepared.chat import get_response
+from prepared.training import train_artificial_intelligence, add_new_intents
+
+def main():
+    path = 'prepared/data.pth'
+    exist_file = Path(path).exists()
+
+    if not exist_file:
+        error = train_artificial_intelligence()
+        print('Bot initialized successfully!')
+        if error != None:
+            return error
+
+    print("GO! Bot is running! (type 'quit' or 'CTRL + C' to exit)")
+
+    try:
+        while True:
+            # chat with 1 question
+            sentence = input("You: ")
+
+            if sentence == "quit":
+                break
+
+            learn, resp = get_response(sentence)
+            print(resp.capitalize())
+            
+            if learn == 1:
+                new_answer = input("Viết thêm câu trả lời hoặc gõ 'skip' để bỏ qua: ")
+                
+                if new_answer == 'skip': continue
+                
+                error = add_new_intents(sentence, new_answer)
+                if error != None:
+                    return error
+
+                error = train_artificial_intelligence()
+                if error != None:
+                    return error
+
+                resp = "Cảm ơn bạn! Tôi đã học được :)"
+                print(resp.capitalize())
+        
+    except KeyboardInterrupt:
+        print("You typed CTRL + C => exit!")
+    
+    except Exception as error:
+        return error
 
 if __name__ == "__main__":
-    print("GO! Bot is running! (type 'quit' to exit)")
-    while True:
-        # sentence = "do you use credit cards?"
-        sentence = input("You: ")
-        if sentence == "quit":
-            break
-
-        resp = get_response(sentence).capitalize()
-        print(resp)
+    error = main()
+    if error != None:
+        print("Error: ", error)
--- prepared/chat.py
+++ prepared/chat.py
@@ -6,30 +6,27 @@ import torch
 from prepared.model import NeuralNet
 from prepared.nltk_utils import bag_of_words, tokenize
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-with open('prepared/intents.json', 'r', encoding="utf8") as json_data:
-    intents = json.load(json_data)
-
-FILE = "prepared/data.pth"
-data = torch.load(FILE)
-
-input_size = data["input_size"]
-hidden_size = data["hidden_size"]
-output_size = data["output_size"]
-all_words = data['all_words']
-tags = data['tags']
-model_state = data["model_state"]
-
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
-model.load_state_dict(model_state)
-model.eval()
-
 bot_name = "HaJun"
 request_name = "name"
-response_exception = "Tôi không hiểu bạn đang nói gì..."
+response_exception = "Tôi không biết điều này. Bạn có thể dạy không?"
 
 def get_response(msg):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    FILE = "prepared/data.pth"
+    data = torch.load(FILE)
+
+    input_size = data["input_size"]
+    hidden_size = data["hidden_size"]
+    output_size = data["output_size"]
+    all_words = data['all_words']
+    tags = data['tags']
+    model_state = data["model_state"]
+
+    model = NeuralNet(input_size, hidden_size, output_size).to(device)
+    model.load_state_dict(model_state)
+    model.eval()
+
     sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
@@ -42,15 +39,25 @@ def get_response(msg):
 
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
+
+    learn = 1
+    with open('prepared/intents.json', 'r', encoding="utf8") as json_data:
+        intents = json.load(json_data)
+
     if prob.item() > 0.75:
         for intent in intents['intents']:
-            if tag == request_name and tag == intent["tag"]:
+            # get name bot
+            if tag == intent["tag"] and tag == request_name:
                 response_random = random.choice(intent['responses']).capitalize()
-                return " ".join([response_random, bot_name])
+                response_name = " ".join([response_random, bot_name])
+                learn = 0
+
+                return learn, response_name
 
             if tag == intent["tag"]:
                 response_random = random.choice(intent['responses']).capitalize()
-                return response_random
+                learn = 0
 
-    return response_exception
+                return learn, response_random
 
+    return learn, response_exception
